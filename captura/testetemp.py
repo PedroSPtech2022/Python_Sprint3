076bad63c1357a21f76208dc324cf7cb813756e2
pedrosptech2022 <pedro.cordeironeto@sptech.school>
2022-11-01 10:30:41 -0300

    att

--- captura/testetemp.py
+++ captura/testetemp.py
@@ -7,23 +7,6 @@ print(type(data))  # dict
 print('--- value ---')
 print(data)
 
-core = data['coretemp']
-print('\n--- type ---')
-print(type(core))  # list
-print('--- value ---')
-print(core)
-
-item = core[0]
-print('\n--- type ---')
-print(type(item))  # object shwtemp
-print('--- value ---')
-print(item)
-print('--- fields ---')
-print('label   :', item.label)
-print('current :', item.current)
-print('high    :', item.high)
-print('critical:', item.critical)
-
 # -------------------------------------------------
 
 data = psutil.sensors_temperatures()
